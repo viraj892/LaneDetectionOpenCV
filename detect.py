ddadf1ec90c793e95a9c5a51695cba393cae7d2b
Hung Do <hungdo554@gmail.com>
2017-04-19 21:03:38 -0400

    Cleaned up code

--- detect.py
+++ detect.py
@@ -1,21 +1,20 @@
 import cv2
-import numpy as np
 
-cap = cv2.VideoCapture.open('solidWhiteRight.mp4')
-print 'after'
+cap = cv2.VideoCapture('mp4/solidWhiteRight.mp4')
+
 while (cap.isOpened()):
-    print 'inside'
+
     # Capture frame-by-frame
     ret, frame = cap.read()
 
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    image = cv2.cvtColor(frame, cv2.COLOR_RGB2RGBA)
 
     # Display the resulting frame
-    cv2.imshow('frame', gray)
+    cv2.imshow('frame', image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-    # When everything done, release the capture
-    cap.release()
-    cv2.destroyAllWindows()
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
